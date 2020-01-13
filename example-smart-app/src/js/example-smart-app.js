e13d848afc160bc35901d6d0ffc42177cb074f05
KnightFuri <47087821+KnightFuri@users.noreply.github.com>
2020-01-14 01:00:43 +0530

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -72,9 +72,9 @@
         
         $.when(pt, enc).fail(onError);
         $.when(pt, enc).done(function(patient, enc) {
-       enc.foreach( encounter => {
-      $('#encounter').append("<tr><td>" + encounter.id + "</td><td> " + encounter.class + "</td></tr>");
-       } );
+       console.log(enc.length);
+      $('#encounter').append("<tr><td>" + enc[0].id + "</td><td> " + enc[0].class + "</td></tr>");
+       
     });
       } else {
         onError();
